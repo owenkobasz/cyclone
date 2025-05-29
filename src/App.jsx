61712fc88d0f9f70a483928440ef1bd557f9934a
Matt Schwartz <matschwartz6.0@gmail.com>
2025-05-29 16:11:20 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+        
+            <div className="bg-blue-100">
+                <h1>Cyclone</h1>
+            </div>
+                
+        </>
+    )
+}
+
+export default App
