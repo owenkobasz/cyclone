f34d49f81ba2e5ecf378e9795e62f185c09dd41b
Owen Kobasz <owenkobasz@gmail.com>
2025-06-06 16:41:29 -0400

    added routes page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+export default {
+    content: ['./index.html', './src/**/*.{js,jsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4ED7F1',
+                secondary: '#6FE6FC',
+                accent: '#A8F1FF',
+                base: '#FFFFFA',
+                highlight: '#FFFF8D',
+            },
+        },
+    },
+    plugins: [],
+}
