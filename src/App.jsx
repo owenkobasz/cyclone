fe4a0ca9929f56d1f784e120f1aeff7c399116a2
Matt Schwartz <matschwartz6.0@gmail.com>
2025-06-12 13:03:50 -0400

    Added updatable map components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import About from './pages/About'
 import RouteDisplay from './pages/RouteDisplay';
+import Map from './pages/Map';
 
 function App() {
     return (
@@ -12,13 +13,15 @@ function App() {
                 <nav className="mt-2">
                     <Link to="/" className="mr-4 text-blue-600">Home</Link>
                     <Link to="/login" className="mr-4 text-blue-600">Login</Link>
-                    <Link to="/routes" className="text-blue-600">Routes</Link>
+                    <Link to="/routes" className="mr-4 text-blue-600">Routes</Link>
+                    <Link to="/map" className="text-blue-600">Maps</Link>
                 </nav>
             </div>
 
             <Routes>
                 <Route path="/" element={<h2 className="p-4">Home Page</h2>} />
                 <Route path="/routes" element={<RouteDisplay />} />
+                <Route path="/map" element={<Map />} />
             </Routes>
         </Router>
     )
