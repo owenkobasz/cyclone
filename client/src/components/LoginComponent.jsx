7f8707a41ddcdbb48f7a24f4af8e0fe40ae2bf14
Matt Schwartz <matschwartz6.0@gmail.com>
2025-07-08 13:01:55 -0400

    Got user auth working, create new users db that stores hashed paswords and username. Registration and login works. Still need to add in session states though.

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -1,31 +1,78 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const LoginComponent = () => {
-    
-    // hooks for username, password, and errors during form submission
-    const [username, setUsername] = "";
-    const [password, setPassword] = "";
-    const [error, setError] = "";
+        
+        // hooks for username, password, and errors during form submission
+        const [username, setUsername] = useState("");
+        const [password, setPassword] = useState("");
+        const [message, setMessage] = useState("");
+        const [loggedin, setLoggedin] = useState(false);
+        const [button, setButton] = useState("");
 
-    // submission function
-    const submit = () => {
-        // check username is valid
-        // check
+        // update login button based on login state
+        useEffect(() => {
+            if (!loggedin) {
+                setButton("Login / Register");
+            } else {
+                setButton("Logout");
+            }
+        },[loggedin]);
 
-    }
-  return (
+        // submission function
+        const submit = async (e) => {
+            // prevent reload
+            e.preventDefault();
+            
+            // try logging in
+            if (!loggedin) {
+                try {
+                    // send a POST request to server
+                    const res = await fetch('http://localhost:3000/api/register', {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({username, password})
+                    });
 
-    <div className='flex justify-center'>
-        <form className='flex flex-col'>
-            <label>Username</label>
-            <input class="outline mb-4" type="text"/>
-            <label>Password</label>
-            <input class="outline mb-4" type="text"/>
-            <input class="outline my-4 hover:bg-amber-200" type="submit" value="Submit!"/>
-        </form>
-    </div>
+                    // set data to response from backend
+                    const data = await res.json();
+
+                    if(!res.ok) {
+                        setMessage(`${data.message}`);
+                    } else {
+                        setMessage(`${data.message}`);
+                        setLoggedin(true);
+                    } 
+                } catch(err) {
+                    setMessage(`Error logging in`);
+                }
+            } else {
+                setLoggedin(false);
+                setUsername("");
+                setPassword("");
+                setMessage("");
+            }
+                       
+        };
+
+    return (
+
+        <div className='flex flex-col justify-center items-center'>
+            <h1 className='pb-4'>Welcome Please Login!</h1>
+                <form className='flex flex-col border p-3 rounded-2xl'
+                onSubmit={submit}>
+                        <label>Username</label>
+                        <input className="outline mb-4" type="text" value={username}
+                        onChange={(e) => setUsername(e.target.value)}/>
+                        <label>Password</label>
+                        <input className="outline mb-4" type="password" value ={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
+                        <p className='text-center'>{message}</p>
+                        <input className="outline my-4 bg-blue-200 hover:bg-amber-200 rounded-2xl" 
+                                type="submit" value={button}/>
+                </form>
+        </div>
  
-  )
+    )
 }
 
 export default LoginComponent
--- server/server.js
+++ server/server.js
@@ -1,32 +1,88 @@
 const express = require('express')
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const dbUsers = require('./dbUsers.js'); // import users database
+const bcrypt = require('bcrypt');
 
+// allow json parsing
+app.use(express.json());
 
 // Enable CORS for all routes
 app.use(cors());
 
-// testing
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/api/register', async (req, res) => {
 
-// create a db
-const db = new sqlite3.Database('./database.sqlite')
-app.post('/api/add', (req,res) => {
-    const info = req.body.info || 'Default Value'
-    db.run('INSERT INTO lorem (info) VALUES (?)', [info], function (err) {
-        if (err) {
-            console.error(err.message)
-            return res.status(500).send('Failed to insert row')
+    // encryption stuff
+    const saltRounds = 10;
+
+    // destructure request
+    const {username, password} = req.body;
+    console.log(username);
+
+    // handle registration / login
+    dbUsers.get(
+        `SELECT * FROM users WHERE username = ?`,
+        [username],
+        (err, row) => {
+            if (err) {
+                console.error('DB error:', err.message);
+                return;
+            } else if (row) {
+                // user exists try and login with given password
+                console.log('Username exists');
+                console.log('DB hashed password:', row.password);
+                bcrypt.compare(password, row.password, (err, match) => {
+                    // handle error
+                    if (err) {
+                        console.error('Error comparing passwords: ', err);
+                        return res.status(500).json({error: 'Login error'});
+                    }
+
+                    // if match
+                    if(match) {
+                        console.log(`${username} succesfully logged in.`);
+                        return res.json({message:"Succesful login", ok: true});
+                    } else {
+                        console.log(`${username} unsuccesfully logged in.`);
+                        return res.status(401).json({ message: 'Incorrect password', ok: false });
+                    }
+                });
+
+
+            } else {
+                // store user into table
+                console.log('Username does not exist');
+                // encrypt password
+                bcrypt.hash(password, saltRounds, function(err, hash) {
+                    if (err) {
+                        console.error('Error hashing password:', err);
+                    return;
+                    }
+
+                    console.log(hash);
+
+                    // Store things in database now
+                    dbUsers.run(`INSERT INTO users (username,password) VALUES (?,?)`,
+                    [username,hash], (err) => {
+                        if(err) {
+                            console.error(err);
+                            return;
+                        }
+                        return res.json({ message: 'User registered and logged in!', ok: true });
+                    }
+
+                    )
+                });
+            }
         }
-        res.send({message:'Row added', id: this.lastID})
-    })
-})
+    );
+});
+
+
 
 // verifies backend has started
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
