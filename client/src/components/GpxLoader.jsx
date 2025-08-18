a6202ccd9c2803f5b334dbe8880b4df17af651e1
Matt Schwartz <matschwartz6.0@gmail.com>
2025-08-18 15:58:54 -0400

    removed some unnecessary files

--- client/src/components/GpxLoader.jsx
+++ client/src/components/GpxLoader.jsx
@@ -1,34 +1,34 @@
-// components/GpxLoader.jsx
-import { useEffect } from 'react';
-import { useMap } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet-gpx';
+// // components/GpxLoader.jsx
+// import { useEffect } from 'react';
+// import { useMap } from 'react-leaflet';
+// import L from 'leaflet';
+// import 'leaflet-gpx';
 
-export default function GpxLoader({ onStatsReady, onCuesReady }) {
-    const map = useMap();
+// export default function GpxLoader({ onStatsReady, onCuesReady }) {
+//     const map = useMap();
 
-    useEffect(() => {
-        const gpx = new L.GPX('/chill_hills.gpx', {
-            async: true,
-            marker_options: { startIconUrl: null, endIconUrl: null, shadowUrl: null },
-            polyline_options: { color: '#4ED7F1', weight: 4 },
-        });
+//     useEffect(() => {
+//         const gpx = new L.GPX('/chill_hills.gpx', {
+//             async: true,
+//             marker_options: { startIconUrl: null, endIconUrl: null, shadowUrl: null },
+//             polyline_options: { color: '#4ED7F1', weight: 4 },
+//         });
 
-        gpx.on('loaded', function (e) {
-            map.fitBounds(e.target.getBounds());
-            onStatsReady({
-                distanceKm: e.target.get_distance() / 1000,
-                elevationM: e.target.get_elevation_gain(),
-            });
-            onCuesReady(
-                e.target.get_segments().flatMap((seg) =>
-                    seg.points.map((pt) => pt.name).filter(Boolean)
-                )
-            );
-        });
+//         gpx.on('loaded', function (e) {
+//             map.fitBounds(e.target.getBounds());
+//             onStatsReady({
+//                 distanceKm: e.target.get_distance() / 1000,
+//                 elevationM: e.target.get_elevation_gain(),
+//             });
+//             onCuesReady(
+//                 e.target.get_segments().flatMap((seg) =>
+//                     seg.points.map((pt) => pt.name).filter(Boolean)
+//                 )
+//             );
+//         });
 
-        gpx.addTo(map);
-    }, [map, onStatsReady, onCuesReady]);
+//         gpx.addTo(map);
+//     }, [map, onStatsReady, onCuesReady]);
 
-    return null;
-}
+//     return null;
+// }
